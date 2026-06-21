1aceb276f6b4435aae375f2c735bbb69588d3134
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-06-21 17:12:47 +0530

    Define Note service interface

---
+++ notes-api/src/main/java/com/nikhilsable/notesapi/services/NoteService.java
@@ -0,0 +1,15 @@
+package com.nikhilsable.notesapi.services;
+
+import com.nikhilsable.notesapi.models.Note;
+
+import java.util.List;
+
+public interface NoteService {
+    Note createNoteForUser(String username, String content);
+
+    Note updateNoteForUser(Long noteId, String content, String username);
+
+    void deleteNoteForUser(Long noteId, String username);
+
+    List<Note> getNotesForUser(String username);
+}
