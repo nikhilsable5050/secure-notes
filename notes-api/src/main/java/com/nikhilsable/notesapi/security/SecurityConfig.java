8d40569d41f6958f84b6082ecaace55e5cc59b66
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-06-29 19:06:05 +0530

    Configure JdbcUserDetailsManager with DataSource

--- notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
+++ notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -27,9 +29,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(DataSource dataSource) {
         JdbcUserDetailsManager manager =
-                new JdbcUserDetailsManager();
+                new JdbcUserDetailsManager(dataSource);
         if (!manager.userExists("user1")) {
             manager.createUser(
                     User.withUsername("user1")
