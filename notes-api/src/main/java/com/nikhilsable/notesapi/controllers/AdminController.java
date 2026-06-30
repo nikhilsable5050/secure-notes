99c8080422a8a7a14cf7ddb855f923bc890c49a8
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-06-30 19:48:04 +0530

    Move authorization logic from controller to service

--- notes-api/src/main/java/com/nikhilsable/notesapi/controllers/AdminController.java
+++ notes-api/src/main/java/com/nikhilsable/notesapi/controllers/AdminController.java
@@ -6,32 +6,38 @@ import com.nikhilsable.notesapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
+//@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class AdminController {
 
     @Autowired
     UserService userService;
 
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/getusers")
     public ResponseEntity<List<User>> getAllUsers() {
-        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+        return new ResponseEntity<>(userService.getAllUsers(),
+                HttpStatus.OK);
     }
 
     @PutMapping("/update-role")
-    public ResponseEntity<String> updateUserRole(@RequestParam Long userId, 
+    public ResponseEntity<String> updateUserRole(@RequestParam Long userId,
                                                  @RequestParam String roleName) {
         userService.updateUserRole(userId, roleName);
         return ResponseEntity.ok("User role updated");
     }
 
+    // @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/user/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
-        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+        return new ResponseEntity<>(userService.getUserById(id),
+                HttpStatus.OK);
     }
 
 
