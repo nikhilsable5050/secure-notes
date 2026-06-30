027e60199efe1abd2d1707954a8756d623cf39c0
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-06-30 14:14:08 +0530

    Define User service interface

---
+++ notes-api/src/main/java/com/nikhilsable/notesapi/services/UserService.java
@@ -0,0 +1,14 @@
+package com.nikhilsable.notesapi.services;
+
+import com.nikhilsable.notesapi.dtos.UserDTO;
+import com.nikhilsable.notesapi.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+}
