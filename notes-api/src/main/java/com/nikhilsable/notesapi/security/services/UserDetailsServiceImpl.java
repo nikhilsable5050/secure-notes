0631232739e56cad76e7b8c2c22bc55205497a7d
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-06-30 13:06:08 +0530

    Implement custom UserDetailsService

---
+++ notes-api/src/main/java/com/nikhilsable/notesapi/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.nikhilsable.notesapi.security.services;
+
+
+import com.nikhilsable.notesapi.models.User;
+import com.nikhilsable.notesapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+
+
+
+}
