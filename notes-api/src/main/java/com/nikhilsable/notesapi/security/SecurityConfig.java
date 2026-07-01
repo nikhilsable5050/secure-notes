e50f2eaf67bf42cfd592cc3b703efcb8abe79140
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-01 14:02:43 +0530

    Enable method-level security

--- notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
+++ notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
@@ -34,8 +34,7 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests)
                 -> requests
-                .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                .requestMatchers("/public/**").permitAll()
+                //.requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
         //http.formLogin(withDefaults());
