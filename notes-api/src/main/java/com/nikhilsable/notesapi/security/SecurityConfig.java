a7b272ffbacbdac84643d480d7485bd8c2039c54
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-02 19:55:24 +0530

    Register custom logging filter in security filter chain

--- notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
+++ notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -39,6 +40,7 @@ public class SecurityConfig {
                 //.requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated());
         http.csrf(AbstractHttpConfigurer::disable);
+        http.addFilterBefore(new CustomLoggingFilter(), UsernamePasswordAuthenticationFilter.class);
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
