15fb55301014c6a62e47065a182d908ca2c0e292
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-03 22:22:40 +0530

    Implement user lookup by username

--- notes-api/src/main/java/com/nikhilsable/notesapi/services/impl/UserServiceImpl.java
+++ notes-api/src/main/java/com/nikhilsable/notesapi/services/impl/UserServiceImpl.java
@@ -8,10 +8,10 @@ import com.nikhilsable.notesapi.repositories.RoleRepository;
 import com.nikhilsable.notesapi.repositories.UserRepository;
 import com.nikhilsable.notesapi.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -65,5 +65,10 @@ public class UserServiceImpl implements UserService {
         );
     }
 
+    @Override
+    public User findByUsername(String username) {
+        Optional<User> user = userRepository.findByUserName(username);
+        return user.orElseThrow(() -> new RuntimeException("User not found with username: " + username));
+    }
 
 }
