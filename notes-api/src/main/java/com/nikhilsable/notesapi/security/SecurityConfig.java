73fe9b92c3860ab20ed34c9e2d96f26235e47e85
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-04 12:30:56 +0530

    Enable CORS in Spring Security

--- notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
+++ notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(withDefaults());
         http.csrf( csrf ->
                 csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                         .ignoringRequestMatchers("/api/auth/public/**")
