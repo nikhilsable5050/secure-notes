4db720a98104535f56b633bce7b1ee2faa5affce
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-04 15:48:03 +0530

    Secure audit log endpoints with admin authorization

--- notes-api/src/main/java/com/nikhilsable/notesapi/controllers/AuditLogController.java
+++ notes-api/src/main/java/com/nikhilsable/notesapi/controllers/AuditLogController.java
@@ -3,6 +3,7 @@ package com.nikhilsable.notesapi.controllers;
 import com.nikhilsable.notesapi.models.AuditLog;
 import com.nikhilsable.notesapi.services.AuditLogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,13 @@ public class AuditLogController {
     AuditLogService auditLogService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<AuditLog> getAuditLogs(){
         return auditLogService.getAllAuditLogs();
     }
 
     @GetMapping("/note/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<AuditLog> getNoteAuditLogs(@PathVariable Long id){
         return auditLogService.getAuditLogsForNoteId(id);
     }
