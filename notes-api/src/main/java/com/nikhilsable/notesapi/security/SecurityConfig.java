1a5799ddaf260a42b4798e09007cbf0249438a25
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-07-05 12:18:27 +0530

    Configure OAuth2 login authentication

--- notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
+++ notes-api/src/main/java/com/nikhilsable/notesapi/security/SecurityConfig.java
@@ -62,7 +62,11 @@ public class SecurityConfig {
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .requestMatchers("/api/csrf-token").permitAll()
                 .requestMatchers("/api/auth/public/**").permitAll()
-                .anyRequest().authenticated());
+                .requestMatchers("/oauth2/**").permitAll()
+                .anyRequest().authenticated())
+                .oauth2Login(auth -> {
+
+                        });
         http.exceptionHandling(exception ->
                 exception.authenticationEntryPoint(unauthorizedHandler));
         http.addFilterBefore(authenticationJwtTokenFilter(),
